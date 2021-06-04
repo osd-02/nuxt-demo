60c92539d7659a43f64e3f3feeebf0c0e913db04
osd-02 <sakanaq0223@gmail.com>
2021-06-05 01:25:36 +0900

    ベースヘッダーのコンポーネント化

--- components/BaseHeading.vue
+++ components/BaseHeading.vue
@@ -1,16 +1,13 @@
 <template>
-  <h2>こんにちは．{{ name }}</h2>
+  <h2 class="font-sans text-gray-800 text-center text-3xl mb-10">
+    <slot />
+  </h2>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  props: {
-    name: {
-      type: String,
-      default: '山田',
-    },
-  },
+  name: 'BaseHeading',
 })
 </script>
 
