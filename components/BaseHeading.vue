131b65b8b5dd34008beda94fc7bc44e115424adb
osd-02 <sakanaq0223@gmail.com>
2021-06-04 23:41:12 +0900

    wrapperのコンポーネント化

---
+++ components/BaseHeading.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>こんにちは．{{ name }}</h2>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: '山田',
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
---
+++ components/LayoutWrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LayoutWrapper',
+})
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  padding-top: 80px;
+  @media (min-width: 768px) {
+    padding-top: 0px;
+  }
+}
+</style>
